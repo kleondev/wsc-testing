744f721ef255adb9ea471689110553ec0a45f982
Kelvin León <kekeleon1995@gmail.com>
2022-02-18 10:29:33 -0400

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EstudiantesModule } from './pages/estudiantes/estudiantes.module';
+import { HomeModule } from './pages/home/home.module';
 import { PersonajesModule } from './pages/personajes/personajes.module';
 import { ProfesoresModule } from './pages/profesores/profesores.module';
 
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then(m => HomeModule)
+  },
   {
     path: 'personajes',
     loadChildren: () => import('./pages/personajes/personajes.module').then(m => PersonajesModule)
