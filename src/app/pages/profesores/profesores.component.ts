ef42877a709b289922e5796e2e7c86a7b06f0625
Kelvin León <kekeleon1995@gmail.com>
2022-02-17 23:55:45 -0400

    Profesores

--- src/app/pages/profesores/profesores.component.ts
+++ src/app/pages/profesores/profesores.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { finalize, Subject, takeUntil } from 'rxjs';
+import { ProfesoresService } from 'src/app/providers/profesores/profesores.service';
 
 @Component({
   selector: 'app-profesores',
@@ -7,9 +12,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfesoresComponent implements OnInit {
 
-  constructor() { }
+  private unsubscribe$ = new Subject<void>();
+  
+  public year = new Date().getFullYear();
+  public displayedColumns: string[] = ['name', 'patronus', 'age', 'image'];
+  public dataSource: MatTableDataSource<any> = new MatTableDataSource(<any>[]);
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  public isLoading = false;
+  
+  public options = [
+    {name: "Slytherin", value: "slytherin"},
+    {name: "Gryffindor", value: "gryffindor"},
+    {name: "Ravenclaw", value: "ravenclaw"},
+    {name: "Hufflepuff", value: "hufflepuff"}
+  ];
+
+  constructor(
+    private profesoresService: ProfesoresService,
+  ) { }
 
   ngOnInit(): void {
+    this.loadProfesores();
   }
 
+  loadProfesores() {
+    this.isLoading = true;
+    this.profesoresService.findAllStaff()
+    .pipe(
+      finalize(() =>  this.isLoading = false),
+      takeUntil(this.unsubscribe$))
+    .subscribe( (response:any) => {
+      this.dataSource.data = response;
+      this.dataSource.paginator = this.paginator;
+      this.initSort();
+      this.dataSource.sort = this.sort;
+    })
+  }
+
+
+  initSort() {
+    this.dataSource.sortingDataAccessor = (data, key) => {
+      switch (key) {
+        case 'name': return data.name;
+        case 'patronus': return data.patronus;
+        case 'age': return data.yearOfBirth > 0 ? data.yearOfBirth : "";
+        default: data[key]
+      }
+    };
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 }
--- src/app/providers/profesores/profesores.service.ts
+++ src/app/providers/profesores/profesores.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,10 @@ import { Injectable } from '@angular/core';
 })
 export class ProfesoresService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  findAllStaff()  {
+    return this.http.get('http://hp-api.herokuapp.com/api/characters/staff');
+  }
+
 }
